6ebe9e6869a1244549ebbb85e2274409f327899c
learningmonster <learningmonster@users.noreply.github.com>
2022-10-17 08:54:18 -0700

    Added mapping to track multiples waves by address

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,14 +6,18 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 totalWaves;
+    mapping(address => uint) waveCountsByAddress;
 
     constructor() {
-        console.log("Yo yo, I am a contract and I am smart");
+        console.log("Yo yo, I am a contract and I am smart.");
+        console.log("It's great to be alive!");
     }
 
     function wave() public {
         totalWaves += 1;
+        waveCountsByAddress[msg.sender] += 1;
         console.log("%s has waved!", msg.sender);
+        console.log("They have waved %d times!", waveCountsByAddress[msg.sender]);
     }
 
     function getTotalWaves() public view returns (uint256) {
